132000230f69d7e7009129c561b1f5cb36c1abad
Josh Cox <josh@webhosting.coop>
2015-12-30 18:52:47 -0600

    bootstrap uses tmp

--- bootstraproustabout.sh
+++ bootstraproustabout.sh
@@ -1,9 +1,13 @@
 #!/bin/sh
-GIT_HOME=~/git
+TMP_DIR=$(mktemp -d --suffix='.bomsaway')
 
 echo roustabout
 cd $GIT_HOME
+cd $TMP_DIR
 git clone https://github.com/joshuacox/roustabout.git
 cd roustabout
 git pull
-make install
+sudo make install
+
+cd
+rm -Rf $TMP_DIR
